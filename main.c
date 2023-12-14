1b37d11d969f3e8f3acb7cdb6b7069aa6f0ff4d5
Amir Murad <amirmurad696@gmail.com>
2023-12-15 00:30:48 +0300

    second task

---
+++ main.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *prompt = NULL;
+	char *token;
+	size_t cmd_size = 0;
+	pid_t pid;
+	int i;
+	char *args[30];
+
+	while (1)
+	{
+		printf("#cisfun$ ");
+		if (getline(&prompt, &cmd_size, stdin) == -1)
+		{
+			if (feof(stdin))
+			{
+				break;
+			}
+			else
+			{
+				perror("Error reading prompt");
+				return (1);
+			}
+		}
+		prompt[strcspn(prompt, "\n")] = '\0';
+		token = strtok(prompt, " ");
+		i = 0;
+		while (token != NULL)
+		{
+			args[i++] = token;
+			token = strtok(NULL, " ");
+		}
+
+		args[i] = NULL;
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("Fork failed");
+			return (1);
+		}
+
+		if (pid == 0)
+		{
+			if (execve(args[0], args, NULL) == -1)
+			{
+				perror("Error");
+				return (1);
+			}
+		}
+		else
+		{
+			int status;
+
+			if (waitpid(pid, &status, 0) == -1)
+			{
+				perror("Wait failed");
+				return (1);
+			}
+		}
+	}
+
+	free(prompt);
+	return (0);
+}
