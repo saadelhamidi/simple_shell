44d13717ba4571feee95be2ae3cb896dfd69c10f
saadelhamidi <saadavicii123@gmail.com>
2024-02-28 12:21:26 -0800

    execution_command.c

--- execution_command.c
+++ execution_command.c
@@ -1,77 +1,28 @@
 #include"shell.h"
-/** 
- *  *
- *   * * main: the primary function
- *    * * @ac: the number of arguments 
- *     * * @argv: arg vector 
- *      * * Return: 0 in all cases.*/
-
+/**
+ *  * _execute_command - function that executes the program referred to by (path).
+ *   * @tokens: array executable.
+ *    * @argv: an array of pointers to strings passed to the new program
+ *     * as its command-line arguments.
+ *      * Return: On success, return 0, on error -1 is returned
+ *       */
 int _execute_command(char **tokens, char **argv)
 {
 		int status = 0, i = 0;
-			pid_t child;
+		pid_t child;
 
-				child = fork();
+		child = fork();
 
-					if (child == 0)
-							{
-										if (execve(tokens[0], tokens, environ) == -1)
-													{
-																	perror(argv[0]);
-																				for (i = 0; tokens[i]; i++)
-																								{
-																													free(tokens[i]), tokens[i] = NULL;
-																																}
-																							free(tokens);
-																										exit(0);
+		if (child == 0)
+	{
+		if (execve(tokens[0], tokens, environ) == -1)
+		{												
+		perror(argv[0]);
+		for (i = 0; tokens[i]; i++)
+	     {
+	               																											free(tokens[i]), tokens[i] = NULL;
 																												}
-											}
-						else
-								{
-											waitpid(child, &status, 0);
-													for (i = 0; tokens[i]; i++)
-																{
-																				free(tokens[i]), tokens[i] = NULL;
-																						}
-															free(tokens);
-																}
-							return (WEXITSTATUS(status));
-}#include"shell.h"
-/** 
- *  *
- *   * * main: the primary function
- *    * * @ac: the number of arguments 
- *     * * @argv: arg vector 
- *      * * Return: 0 in all cases.*/
-
-int _execute_command(char **tokens, char **argv)
-{
-		int status = 0, i = 0;
-			pid_t child;
-
-				child = fork();
-
-					if (child == 0)
-							{
-										if (execve(tokens[0], tokens, environ) == -1)
-													{
-																	perror(argv[0]);
-																				for (i = 0; tokens[i]; i++)
-																								{
-																													free(tokens[i]), tokens[i] = NULL;
-																																}
-																							free(tokens);
-																										exit(0);
+		free(tokens);
+		exit(0);
 																												}
-											}
-						else
-								{
-											waitpid(child, &status, 0);
-													for (i = 0; tokens[i]; i++)
-																{
-																				free(tokens[i]), tokens[i] = NULL;
-																						}
-															free(tokens);
-																}
-							return (WEXITSTATUS(status));
-}
+	}
